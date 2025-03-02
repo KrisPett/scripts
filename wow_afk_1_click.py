aaa18a63a122403972b1a46f81237e35c654c9f0
KrisPett <70701718+KrisPett@users.noreply.github.com>
2025-03-02 20:58:23 +0100

    commit

--- wow_afk_1_click.py
+++ wow_afk_1_click.py
@@ -2,7 +2,7 @@ import pyautogui
 import time
 
 if __name__ == "__main__":
-    print("Pressing '1' every second...")
+    print("Pressing '1' every 100 milliseconds...")
     while True:
         pyautogui.press('1')
         time.sleep(0.30)
